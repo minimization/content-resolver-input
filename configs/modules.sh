09400c264bf2caec34c749c20d6615096bd5800c
Adam Samalik <asamalik@redhat.com>
2021-02-12 11:58:52 +0100

    don't use modules in c9s

---
+++ configs/modules.sh
@@ -0,0 +1,6 @@
+for filename in sst_cs_infra_services-varnish sst_cs_apps-java-ant sst_cs_apps-perl-5.30 sst_cs_infra_services-subversion sst_cs_infra_services-nginx sst_cs_apps-perl-5.32 sst_dotnet_java-jmc sst_cs_apps-java-maven stream-httpd  
+do
+
+mv $filename-c9s.yaml $filename.yaml
+
+done
